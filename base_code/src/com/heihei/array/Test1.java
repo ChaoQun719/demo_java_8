b03aa24a0b6521dc65ac1ce97962c4417ea02d98
chaoqun <zhizhuo0476@163.com>
2026-05-25 15:56:18 +0800

    reset project

---
+++ base_code/src/com/heihei/array/Test1.java
@@ -0,0 +1,9 @@
+package com.heihei.array;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int[][] arr1 = new int[4][3];
+        System.out.println(arr1[0]);
+        System.out.println(arr1[0][1]);
+    }
+}
